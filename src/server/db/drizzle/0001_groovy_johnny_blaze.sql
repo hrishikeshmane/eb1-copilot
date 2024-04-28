bd6d6a632620154b32ec19331b1732c5a4302bad
hrishikeshmane <hrishi.mane26@gmail.com>
2024-04-28 15:40:01 -0400

    reaf: moving to new db arch

--- src/server/db/drizzle/0001_groovy_johnny_blaze.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE eb1-copilot_userInfo ADD `currentVisa` text NOT NULL;--> statement-breakpoint
-ALTER TABLE eb1-copilot_userInfo ADD `isStudent` integer NOT NULL;--> statement-breakpoint
-ALTER TABLE eb1-copilot_userInfo ADD `graduationYear` text NOT NULL;--> statement-breakpoint
-ALTER TABLE eb1-copilot_userInfo ADD `currentRole` text(200) NOT NULL;--> statement-breakpoint
-ALTER TABLE eb1-copilot_userInfo ADD `industryType` text NOT NULL;--> statement-breakpoint
-ALTER TABLE eb1-copilot_userInfo ADD `priorityDateIfAny` text(50);--> statement-breakpoint
-ALTER TABLE `eb1-copilot_userInfo` DROP COLUMN `everBeenJ1OrJ2`;--> statement-breakpoint
-ALTER TABLE `eb1-copilot_userInfo` DROP COLUMN `haveCriminalRecord`;
