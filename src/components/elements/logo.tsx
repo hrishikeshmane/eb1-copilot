e6711a65a15b8bce48e3af448dc53f1b40d82836
hrishikeshmane <hrishi.mane26@gmail.com>
2024-05-12 00:30:46 -0400

    fat: new logo

--- src/components/elements/logo.tsx
+++ src/components/elements/logo.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center gap-2 font-semibold">
-      <Image src="/logo-256.png" alt="logo" width={40} height={40} />
+      <Image src="/logo-512.png" alt="logo" width={45} height={45} />
       <span className="text-lg font-bold">Greencard Inc</span>
     </Link>
   );
