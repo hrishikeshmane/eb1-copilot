7acceaae27aebe7a300a7fbf2a078002cf70e5fe
hrishikeshmane <hrishi.mane26@gmail.com>
2024-05-12 00:39:16 -0400

    feat: 404 svg

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,15 @@
 import Header from "@/components/elements/header";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Logo from "@/../public/404.svg";
 
 function NotFoundPage() {
   return (
     <main className="h-full w-full">
       <Header />
       <div className="flex h-[calc(100vh-150px)] w-screen flex-col items-center justify-center">
-        <h1 className="text-8xl font-black">404</h1>
+        <Logo className="h-[20rem]" />
+        <h1 className="text-7xl font-black">404</h1>
         <h2 className="text-lg">Page not found</h2>
         <div className="pt-4">
           <Link href="/">
