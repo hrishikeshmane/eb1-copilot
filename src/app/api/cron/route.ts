ba26d77fac0d2a5f34b38e19f468f0464f8b5113
Akash Mali <akashbm08@gmail.com>
2024-10-12 23:36:33 -0400

    time log added to cron

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -3,22 +3,29 @@ import type { NextRequest } from 'next/server';
 
 export const dynamic = 'force-dynamic';
 
- 
 export async function GET(request: NextRequest) {
-  
-//   const authHeader = request.headers.get('authorization');
-//   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-//     return new Response('Unauthorized', {
-//       status: 401,
-//     });
-//   }
+  // Uncomment the following block if you want to implement authorization
+  // const authHeader = request.headers.get('authorization');
+  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //   return new Response('Unauthorized', {
+  //     status: 401,
+  //   });
+  // }
+
+  const startTime = Date.now(); 
 
   try {
     await sendEmail();
-    console.log("Cron for Not-Onboarded/PriorityCall emails Done!");
-    return new Response('Emails sent successfully', {status: 200});
+    const endTime = Date.now(); 
+    const duration = endTime - startTime;
+
+    const minutes = Math.floor(duration / 60000);
+    const seconds = Math.floor((duration % 60000) / 1000);
+
+    console.log(`Cron for Not-Onboarded/PriorityCall emails done in ${minutes} minutes and ${seconds} seconds!`);
+    return new Response('Emails sent successfully', { status: 200 });
   } catch (error) {
-    console.error('Error sending Not-Onboarded/PriorityCall emails emails:', error);
+    console.error('Error sending Not-Onboarded/PriorityCall emails:', error);
     return new Response(`Failed to send Not-Onboarded/PriorityCall emails: ${error}`, { status: 500 });
   }
 }
