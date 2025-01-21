cead02d41b41281dcd9a394aebaf734b554069bb
hrishikeshmane <hello@greencard.inc>
2025-01-20 17:33:24 -0800

    fix: add navbar back again

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      {/* <Navbar2 /> */}
+      <Navbar2 />
       <div className="flex flex-col sm:gap-4 sm:py-0 sm:pl-14">
         <NavSheet />
         <main className="h-full w-full">{children}</main>
